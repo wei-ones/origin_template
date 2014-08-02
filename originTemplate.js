422ee67c0e17e930d84dfdce624479a54f3ead8f
Weiyoung-Zhou <Weiyoung.Zhou@gmail.com>
2014-08-02 16:02:11 +0800

    update

---
+++ originTemplate.js
@@ -0,0 +1,107 @@
+var KEYWORDS =
+	//
+	'break,case,catch,continue,debugger,default,delete,do,else,end,false' 
+	+ ',finally,for,function,if,in,instanceof,new,null,return,switch,this' 
+	+ ',throw,true,try,typeof,var,void,while,with'
+	// 
+	+ ',abstract,boolean,byte,char,class,const,double,enum,export,extends' 
+	+ ',final,float,goto,implements,import,int,interface,long,native' 
+	+ ',package,private,protected,public,short,static,super,synchronized' 
+	+ ',throws,transient,volatile'
+	// 
+	+ ',arguments,let,yield'
+	//
+	+',undefined';
+
+var commentRe = /\/\*[\w\W]*?\*\/|\/\/[^\n]*\n|\/\/[^\n]*$/g;
+var elseRe = /[\.\[]\w+\]?|\bvar\s+\w+/g;
+var splitRe = /[^\w]+/g;
+var keywordsRe = new RegExp(["\\b" + KEYWORDS.replace(/,/g, '\\b|\\b') + "\\b"].join('|'), 'g');
+var numberRe = /^\d[^,]*|,\d[^,]*/g;
+var boundaryRe = /^,+|,+$/g;
+
+var getVariable = function(templateStr) {
+	return templateStr.replace(commentRe, '').replace(elseRe, '').replace(splitRe, ',').replace(keywordsRe, '').replace(numberRe, '').replace(boundaryRe, '').split(/^$|,+/);
+};
+
+var templateReg = new RegExp(/<%.*?%>/g);
+var getVariables = function(templateStr) {
+	var strList = templateStr.match(templateReg);
+	var vars = [];
+	var hasDict = {},
+		reVars = [];
+	for (var each in strList) {
+		vars = getVariable(strList[each]);
+		for (var index in vars) {
+			if (!hasDict[vars[index]]) {
+				reVars.push(vars[index]);
+				hasDict[vars[index]] = true;
+			};
+		};
+	}
+	return reVars;
+};
+
+var cstartReg = new RegExp(/<%/g);
+var cendReg = new RegExp(/%>/g);
+var valueReg = new RegExp(/<%=\s*(.+?)\s*%>/g);
+var addStr = "htmlStr += '?';";
+
+var thisTemplate = {};
+
+var compile = function(templateName,templateStr) {
+	templateStr = templateStr.replace(/\s+/g, " ").replace(/\n+/g, " ").replace(/'/g, '\\\'').replace(/"/g, '\\\"');
+
+	var vars = getVariables(templateStr);
+
+	var valueRe = valueReg.exec(templateStr);
+	while (valueRe) {
+		templateStr = templateStr.replace(valueRe[0], "'+" + valueRe[1] + "+'");
+		var valueRe = valueReg.exec(templateStr);
+	}
+
+	var funcStr = "'use strict';";
+	funcStr += "var htmlStr=''";
+	for (each in vars) {
+		funcStr += "," + vars[each] + "=" + "args." + vars[each];
+	}
+	funcStr += ";";
+
+	var startRe = cstartReg.exec(templateStr);
+	var endRe = cendReg.exec(templateStr);
+	var lastPos = 0;
+	while (startRe && endRe) {
+		tempStr = templateStr.slice(lastPos, startRe.index);
+		funcStr += tempStr ? addStr.replace(/\?/, tempStr) : "";
+		funcStr += templateStr.slice(startRe.index + 2, endRe.index);
+		lastPos = endRe.index + 2;
+		startRe = cstartReg.exec(templateStr);
+		endRe = cendReg.exec(templateStr);
+	}
+
+	tempStr = templateStr.slice(lastPos);
+	funcStr += tempStr ? addStr.replace(/\?/, tempStr) : "";
+	funcStr += "return htmlStr;";
+	funcStr = funcStr.replace(/\s+/, " ");
+
+	try {
+		new Function("args", funcStr);
+	} catch (error) {
+		console.log(funcStr);
+		console.log(error.name);
+		console.log(error.number);
+		console.log(error.description);
+		console.log(error.message);
+		return false;
+	}
+
+	return thisTemplate[templateName] =  new Function("args", funcStr);
+};
+
+var renderFromjs = function(templateName,args){
+	return templateDict[templateName](args);
+};
+
+var render = function(templateName,args){
+	return thisTemplate[templateName](args);
+};
